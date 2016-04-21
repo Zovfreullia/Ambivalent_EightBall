39e1b31fe9e9ee073fa1b4dd3041eba33aa1dfa4
Kaira <fzf.villanueva@gmail.com>
2016-04-21 00:21:04 -0400

    updated with bootstrap

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   // start writing your magic 8 ball logic here!
 
     $('#eight-ball').submit(function(e){
-    	var answers = ["Maybe", "Eh...", "Hmmm..."];
+    	var answers = ["Maybe", "Eh...", "Hmmm...", "Wow", "Da Fuck?!"];
     	var eightBall =  answers[Math.floor(Math.random() * answers.length)];
     	$('div#answer').text(eightBall).fadeIn(5000);
     	e.preventDefault();
