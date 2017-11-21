a613e8796fd269c724a59485269804a75cc5c3df
Kaira Villanueva <fzf.villanueva@gmail.com>
2017-11-21 03:42:43 -0500

    woo

--- README.md
+++ README.md
@@ -7,5 +7,3 @@
 What happens when your fortune teller is a nihlist? Well, this is what happens-- the ambivalent eight ball. Answers are randomized and users can ask as many questions as they want.
 
 <sub>WARNING: Please do not take the advice seriously, <i>seriously</i>.</sub>
-
-![eightball](https://media.giphy.com/media/NBj4shBpqpz44/giphy.gif)
--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
 
-  // start writing your magic 8 ball logic here!
-
-    $('#eight-ball').submit(function(e){
-    	var answers = ["Maybe", "Eh...", "Hmmm...", "Wow", "Da Fuck?!"];
-    	var eightBall =  answers[Math.floor(Math.random() * answers.length)];
-    	$('div#answer').text(eightBall).fadeIn(5000);
-    	e.preventDefault();
-	});
+  $('#eight-ball').submit(function(e){
+    var answers = ["Maybe", "Eh...", "Hm...", "Wow", "Fuck?!"];
+    var eightBall =  answers[Math.floor(Math.random() * answers.length)];
+    $('div#answer').text(eightBall).fadeIn(5000);
+    e.preventDefault();
+  });
 
 });
